49710d261b60621f4435ed5b8ced1f2609c5613e
Klybik <sasha.klybik@gmail.com>
2017-08-04 15:44:58 +0300

    minor changes in sql and view

--- getMessages.php
+++ getMessages.php
@@ -1,10 +1,10 @@
 <?php
 include 'connectDB.php';
-
-$query = 'SELECT * FROM messengerdb.messages WHERE sender_id=' . $_SESSION['id'].' AND recipient_id='.$_POST['recipient_id'];
+$query = 'SELECT * FROM messengerdb.messages WHERE sender_id=' . $_SESSION['id'] . ' AND recipient_id=' . $_POST['recipient_id'] . ' UNION SELECT * FROM messengerdb.messages WHERE sender_id=' . $_POST['recipient_id'] . ' AND recipient_id=' . $_SESSION['id'];
 $result = mysqli_query($link, $query);
 
-while ($row = mysqli_fetch_assoc($result)){
+while ($row = mysqli_fetch_assoc($result)) {
     $LIST[] = $row;
 }
-echo json_encode($LIST,JSON_UNESCAPED_UNICODE);
+
+echo json_encode($LIST, JSON_UNESCAPED_UNICODE);
--- setMessages.php
+++ setMessages.php
@@ -1,7 +1,7 @@
 <?php
 include 'connectDB.php';
 
-$query = 'INSERT INTO messengerdb.messages (recipient_id,sender_id,message) VALUES ('.$_POST['recipient_id'].','. $_SESSION['id'].','.$_POST['text'].')';
+$query = 'INSERT INTO messengerdb.messages (recipient_id,sender_id,message) VALUES ('.$_POST['recipient_id'].','. $_SESSION['id'].',\''.$_POST['text'].'\')';
 mysqli_query($link, $query);
-echo mysqli_error();
+echo mysqli_error($link);
 
